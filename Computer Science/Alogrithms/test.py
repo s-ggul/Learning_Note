654443bdd4ab34d1ffb6fa0985adf930fc3d44b1
s-ggul <k0s0a7@gmail.com>
2022-04-13 17:47:49 +0900

    [add] 문제 참고

--- Computer Science/Alogrithms/test.py
+++ Computer Science/Alogrithms/test.py
@@ -10,30 +10,32 @@ l = 2 ** N
 
 q = deque()
 
-def recursiveZ (x, y, l, count):
+def recursiveZ (x, y, l, startIndex):
     global answer
     if l == 2:
         if (x,y) == (r,c):
-            answer = count
+            answer = startIndex
         elif (x,y+1) == (r,c): 
-            answer = count + 1
+            answer = startIndex + 1
         elif (x+1,y) == (r,c): 
-            answer = count + 2
+            answer = startIndex + 2
         elif (x+1,y+1) == (r,c): 
-            answer = count + 3 
+            answer = startIndex + 3 
 
     elif l > 2 :
         dividedL = l//2
         if r < dividedL and c < dividedL:
-            recursiveZ(x ,y, dividedL, count)
+            recursiveZ(x ,y, dividedL, startIndex)
         elif r < dividedL and c >= dividedL:
-            recursiveZ(x ,y+dividedL,dividedL, count+(dividedL**2))
+            recursiveZ(x ,y+dividedL,dividedL, startIndex+(dividedL**2))
         elif r >= dividedL and c < dividedL:
-            recursiveZ(x+dividedL, y,dividedL, count + (dividedL**2) * 2)
+            recursiveZ(x+dividedL, y,dividedL, startIndex + (dividedL**2) * 2)
         elif r >= dividedL and c >= dividedL:
-            recursiveZ(x+dividedL, y+dividedL,dividedL, count+ (dividedL**2) * 3)
+            recursiveZ(x+dividedL, y+dividedL,dividedL, startIndex+ (dividedL**2) * 3)
 answer = 0
 
 recursiveZ(0,0,l,0)
 
 print(answer)
+
+# 분할정복 & 재귀 https://ggasoon2.tistory.com/11
