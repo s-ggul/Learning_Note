0dc0b84c471a0c4ca6211de6f08f3725c1ff0a32
s-ggul <k0s0a7@gmail.com>
2022-04-18 13:23:02 +0900

    [add] 오픈채팅방

--- Computer Science/Alogrithms/test2.py
+++ Computer Science/Alogrithms/test2.py
@@ -1,4 +1,5 @@
-from collections import deque
+'''
+2018 카카오 blind recruitment
 
 def solution(s):
     result = s
@@ -20,15 +21,43 @@ def solution(s):
             if base == word:
                 count += 1
             else:
-                temp += str(count) + base
+                if count == 1:
+                    temp += base
+                else: 
+                    temp += str(count) + base
                 count = 1
                 base = word
-
-        temp = temp.replace('1', '')
-        print(temp)
+        # 마지막에 1을 replace해주는 방식으로 작성했었는데 갯수가 10 ~ 19 인 경우에도 1을 없애서 에러가 나왔다. 조심하자
         if len(result) > len(temp):
             result = temp
 
     return len(result)
+'''
+'''
+2019 KAKAO BLIND RECRUITMENT - 오픈채팅방
 
-print(solution(''))
+def solution(record):
+    opr =[]
+    uidName = {}
+    answer = []
+    
+    for data in record:
+        words = list(data.split())
+        if words[0] == 'Enter': 
+            opr.append((words[0], words[1]))
+            uidName[words[1]] = words[2]
+        elif words[0] == 'Change':
+            uidName[words[1]] = words[2]
+        else:
+            opr.append((words[0], words[1]))
+
+    for k,v in opr:
+        if k =='Enter':
+            answer.append(uidName[v]+'님이 들어왔습니다.')
+        elif k == 'Leave':
+            answer.append(uidName[v]+'님이 나갔습니다.')
+
+    return answer
+
+'''
+print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
