2990be5ce7996b7c061e98ff07f4402a5ca4caa9
s-ggul <k0s0a7@gmail.com>
2022-04-21 20:44:37 +0900

    [add] 카카오 괄호변환 문제풀이 준비

--- Computer Science/Alogrithms/test.js
+++ Computer Science/Alogrithms/test.js
@@ -1,2 +1,15 @@
+/*
+코딩테스트 연습
+2020 KAKAO BLIND RECRUITMENT
+괄호 변환
+https://programmers.co.kr/learn/courses/30/lessons/60058?language=javascript
+*/
 
-console.log(solution());
+function solution(p) {
+    var answer = '';
+    return answer;
+}
+
+console.log(solution("(()())()"));
+console.log(solution(")("));
+console.log(solution("()))((()"));
