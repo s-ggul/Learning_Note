d01ff3aac5b27a77dded86cd0190b4fea299e708
s-ggul <k0s0a7@gmail.com>
2022-04-22 15:39:25 +0900

    [add] test.js

--- Computer Science/Alogrithms/test.js
+++ Computer Science/Alogrithms/test.js
@@ -93,13 +93,117 @@ function checkOk(p)
 /*
 2018 KAKAO BLIND RECRUITMENT - [1차] 뉴스 클러스터링
 
- */
+- 다중 집합의 원리를 잘 파악해야 했음 
+
+function divide(p)
+{
+    let temp = [];
+    let reg = new RegExp(/[a-z][a-z]/);
+
+    for (let i =0; i < p.length-1; i ++){
+        var data = p.slice(i,i+2);
+
+        if (reg.test(data)){
+            temp.push(data);
+        }
+    }
+    return temp
+}
+
 
 function solution(str1, str2) {
     var answer = 0;
+    let strD1 = divide(str1.toLowerCase()); 
+    let strD2 = divide(str2.toLowerCase());
+    let temp = strD2.slice();//strD2를 깊은 복사
+
+    let intersection = [...strD1].filter(x => {
+        if (temp.includes(x))
+        {
+            temp.splice(temp.indexOf(x),1)
+            return true;
+        }
+        else{
+            return false;
+        }
+    });
+
+    let tempInter = intersection.slice();
+    //intersection을 깊은 복사
+
+    let total = [...strD1].filter(x => {
+        if (tempInter.includes(x))
+        {
+            tempInter.splice(tempInter.indexOf(x),1);
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }).concat(strD2);
+
+    
+    if (total.length !== 0){
+        answer = (intersection.length/total.length) * 65536;
+    }
+    else{
+        answer = 65536;
+    }
+
+    return Math.floor(answer);
+}
+ */
+/*
+2020 카카오 인턴십 - 수식 최대화
+
+*/
+function solution(expression) {
+    var orders = [
+        ['*', '+', '-'],
+        ['*', '-', '+'],
+        ['+', '-', '*'],
+        ['+', '*', '-'],
+        ['-', '+', '*'],
+        ['-', '*', '+'],
+    ];
+    let answer = -1;
+    for (const order of orders){
+        var res = parseInt(recursive(order,0,expression));
+        if (res < answer)
+        {
+            answer = res;
+        }
+    }
+
+
     return answer;
 }
 
-console.log(solution("()))((()"));
-console.log(solution("(()())()"));
-console.log(solution(")("));
+function recursive(order,n, expr)
+{
+    let temp = [];
+    if (n == 2)
+    {
+        return eval(expr);
+    }
+    else
+    {
+        if (order[n] == '*')
+        {
+            
+        }
+        else if(order[n] == '-')
+        {   
+            
+        }
+        else if(order[n] == '+')
+        {
+            
+        }
+    }
+    return temp;
+}
+
+console.log(solution("100-200*300-500+20"));
+console.log(solution("50*6-3*2"));
