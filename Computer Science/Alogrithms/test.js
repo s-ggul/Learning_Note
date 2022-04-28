6c95975549059ec6f0704cad3bf3c6f3879e8ba3
s-ggul <k0s0a7@gmail.com>
2022-04-28 14:44:46 +0900

    [fix] files

--- Computer Science/Alogrithms/test.js
+++ Computer Science/Alogrithms/test.js
@@ -1,75 +1,6 @@
 /*
 코딩테스트 연습
 2021 KAKAO BLIND RECRUITMENT
-메뉴 리뉴얼
-- 보류 - 순열 알고리즘 짜야함 
-
-
-let data = new Map();
-
-function solution(orders, course) {
-    var answer = [];
-
-    for (const num of course){
-        for (const order of orders)
-        {
-            dataParse(order, num);
-        }
-    }
-    // 데이터 조합을 찾아 맵핑해주는 함수
-
-
-    data = [...data].sort((a,b) => b[1] - a[1]);
-
-    for (const num of course){
-        let maxCount = -1;
-        for (const e of data){
-            if (e[0].length == num)
-            {
-                if (maxCount == -1)
-                {
-                    maxCount = e[1];
-                    answer.push(e[0]);
-                }
-                else{
-                    if (e[1] == maxCount)
-                    {
-                        answer.push(e[0]);
-                    }
-                    else if(e[1] < maxCount)
-                    {
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    answer = answer.sort();
-    return answer;
-}
-
-function dataParse(s, num){
-    s = [...s].sort();
-    
-    for (let i =0; i<s.length; i ++)
-    {
-        for(let j = i+1; j<s.length;j++)
-        {
-            data.set(`${[s[i]+s[j]]}`, data.get(`${[s[i]+s[j]]}`) + 1 || 1);
-            // 배열 그대로 사용하게되면 주솟값을 맵핑하는 것 같음 따라서 String으로 변환하고자 JS 템플릿 리터럴을 사용
-        }
-    }
-}
-
-//console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]));
-//console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]));
-console.log(solution(["XYZ", "XWY", "WXA"], [2,3,4]));
-*/
-
-/*
-코딩테스트 연습
-2021 KAKAO BLIND RECRUITMENT
 순위 검색
 
 // 이 문제도 순열과 조합을 이용하긴 함=> 조합 혹은 순열을 이용해 해당 info의 가능한 모든 조합을 저장해서 탐색하는 방식으로 진행시킨다.
@@ -108,12 +39,10 @@ console.log(solution(["java backend junior pizza 150","python frontend senior ch
 
 */
 
-// 순열과 조합 참고 : https://jun-choi-4928.medium.com/javascript%EB%A1%9C-%EC%88%9C%EC%97%B4%EA%B3%BC-%EC%A1%B0%ED%95%A9-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-%EA%B5%AC%ED%98%84%ED%95%98%EA%B8%B0-21df4b536349
-
 /*
 2020 KAKAO BLIND RECRUITMENT
 자물쇠와 열쇠
-*/
+
 
 function solution(key, lock) {
     var answer = true;
@@ -157,3 +86,35 @@ function lockToKey(lock)
 }
 
 console.log(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],[[1, 1, 1], [1, 1, 0], [1, 0, 1]]));
+*/
+
+/*
+코딩테스트 연습
+2021 KAKAO BLIND RECRUITMENT
+메뉴 리뉴얼
+- 보류 - 순열 알고리즘 짜야함 
+*/
+
+let data = new Map();
+
+function solution(orders, course) 
+{
+    let answer = [];
+    return answer;
+}
+
+
+function combination(s, n){
+
+    if (n == 1)
+    {
+        return [...s].map((v) => [v]);
+    }
+
+
+
+}
+
+//console.log(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]));
+//console.log(solution(["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"], [2,3,5]));
+console.log(solution(["XYZ", "XWY", "WXA"], [2,3,4]));
