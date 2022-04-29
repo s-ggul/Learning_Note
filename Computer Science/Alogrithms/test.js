c0df6826e968818dff5a2c430d4a55ef187a8710
s-ggul <k0s0a7@gmail.com>
2022-04-29 15:17:20 +0900

    0429

--- Computer Science/Alogrithms/test.js
+++ Computer Science/Alogrithms/test.js
@@ -52,42 +52,47 @@ console.log(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]],[[1, 1, 1], [1, 1, 0], [1
 2021 KAKAO BLIND RECRUITMENT
 순위 검색
 
-// 이 문제도 순열과 조합을 이용하긴 함=> 조합 혹은 순열을 이용해 해당 info의 가능한 모든 조합을 저장해서 탐색하는 방식으로 진행시킨다.
+저는 카카오 문제 풀이 보고 풀었어요!
+info를 순회하면서 문자열 파싱 후에 모든 경우의 수(16가지=24 )에 점수를 넣는다.
+ex. "javabackendjuniorpizza" [150], "-backendjuniorpizza" [150], ... ,
+(저는 중복된 것은 알아서 넣어지도록 저는 C++의 map 컨테이너(key, value) 사용했습니다, value는 vector<int>)
 
-*/
+모든 map의 key에 대해서 순회하면서 오름차순 정렬(처음에 빨리 짜다가 query 순회하면서 했다가 시간초과 났어요... 먼저 해주세요!)
+
+query를 순회하면서 query 정보를 문자열 파싱.
+
+해당 문자열을 key로 map에 접근해서 정렬돼있는 배열 뽑아와서 이분탐색.
+
+요렇게 했더니 제가 푼 C++, JavaScript는 통과했습니다.
+도움 되시길 바라겠습니다!
+
+
 function solution(info, query) {
-    let answer = [];
-    let lang = ['cpp','java','python','-'];
-    let part = ['backend', 'frontend', '-'];
-    let career = ['junior', 'senior', '-'];
-    let food = ['chicken', 'pizza', '-'];
-    let data = new Map();
-
-    for (const l of lang)
-    {
-        for(const p of part)
-        {
-            for(const c of career)
-            {
-                for(const f of food)
-                {
-                    data.set(l+p+c+f, []);
-                }
-            }
-        }
-    }
-
-    console.log(data)
-    return answer;
+    info.forEach((fixed, index, origin) => {
+        
+    });
 }
 
-/*
-1. 먼저 모든 경우에대한 문자열 조합을 만든다. 이때 combination이 아닌 `-`을 포함한 문자열 
-2. 그후 각각의 문자열 별로 점수리스트를 만든다. 
-3. 해당 점수 리스트에서 이분탐색을 진행한다. 
+function binarySearch(arr, score){
+    let low = 0;
+    let high = arr.length -1;
+    let mid;
 
-굿굿
-이렇게 해보장자라장자자라자라장!
-*/
+    while (low <= high)
+    {
+        mid = Math.floor(low + high / 2);
+
+        if (score > arr[mid])
+        {
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+    return low;
+}
 
 console.log(solution(["java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"],["java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"]));
+
+*/
