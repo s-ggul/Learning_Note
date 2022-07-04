05f0c91b23c826ae3fcb87b404e4758f2f30b082
s-ggul <k0s0a7@gmail.com>
2022-07-05 04:49:33 +0900

    SSAFY start

--- Computer Science/Alogrithms/test.py
+++ Computer Science/Alogrithms/test.py
@@ -1,37 +1,9 @@
-import sys 
+n = int(input())
 
-n = int(sys.stdin.readline())
-s = set()
 
-for i in range(n) : 
-    str = sys.stdin.readline().rstrip()
-    cmd = ''
-    num = 0
 
-    if str.find(' ') != -1 :
-        cmd, num = str.split(' ');
-        num = int(num)
-    else:
-        cmd = str
-    
-    if cmd == 'add':
-        s.add(num)
-    elif cmd == 'remove':
-        if num in s:
-            s.remove(num)
-    elif cmd == 'check':
-        if num in s:
-            sys.stdout.write('1\n')
-        else:
-            sys.stdout.write('0\n')
-    elif cmd == 'toggle' : 
-        if num in s:
-            s.remove(num)
-        else:
-            s.add(num)
-    elif cmd == 'all' :
-        s = set([i for i in range(1,21)])
-    elif cmd == 'empty' :
-        s = set()
+
+
+# 총 가짓수를 10007로 나눈 나머지를 출력
 
     
